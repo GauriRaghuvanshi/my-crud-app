ad97109951dd2bef9d4084be7960d8393ccd4e2a
GauriRaghuvanshi <gauriraghuvanshi03@gmail.com>
2025-02-13 17:53:44 +0530

    code updated

--- src/pages/_add.js
+++ src/pages/_add.js
@@ -1,6 +1,6 @@
-import '../styles/globals.css';
 
-import { QueryClient, QueryClientProvider } from 'react-query';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 
